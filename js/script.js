9024b3a5d8d9028d52ae57ed0e280ff7b1d41c80
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-18 16:13:37 +0200

    [UPD] added the array inside the app

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-// DEFINISCO L'ARRAY DI OGGETTI
-const todolist = [
-    {
-        text: 'Lezione della mattina 09:30 - 13:00',
-        done: true,
-    },
-    {
-        text: 'Allenamento',
-        done: false,
-    },
-    {
-        text: 'Esercizi pomeridiani',
-        done: false,
-    },
-    {
-        text: 'Pausa caffe',
-        done: false,
-    },
-    {
-        text: 'Accompagnare Elisa a fare delle commissioni',
-        done: true,
-    },
-    {
-        text: 'Editare video per il 18',
-        done: false,
-    },
-    {
-        text: 'Uscire con Argo',
-        done: true,
-    },
-    
-]
+
 // CREO L'SPA VUE
 const { createApp } = Vue;
 
 createApp({
     data(){
         return{
-
+            // DEFINISCO L'ARRAY DI OGGETTI
+            todolist : [
+                {
+                    text: 'Lezione della mattina 09:30 - 13:00',
+                    done: true,
+                },
+                {
+                    text: 'Allenamento',
+                    done: false,
+                },
+                {
+                    text: 'Esercizi pomeridiani',
+                    done: false,
+                },
+                {
+                    text: 'Pausa caffe',
+                    done: false,
+                },
+                {
+                    text: 'Accompagnare Elisa a fare delle commissioni',
+                    done: true,
+                },
+                {
+                    text: 'Editare video per il 18',
+                    done: false,
+                },
+                {
+                    text: 'Uscire con Argo',
+                    done: true,
+                },
+                
+            ]
         }
     },
     methods: {
