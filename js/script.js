2c474099d61017e28aed95b3ca917f23f22d3516
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-18 16:08:59 +0200

    [UPD] defined the object array

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// DEFINISCO L'ARRAY DI OGGETTI
+const todolist = [
+    {
+        text: 'Lezione della mattina 09:30 - 13:00',
+        done: true,
+    },
+    {
+        text: 'Allenamento',
+        done: false,
+    },
+    {
+        text: 'Esercizi pomeridiani',
+        done: false,
+    },
+    {
+        text: 'Pausa caffe',
+        done: false,
+    },
+    {
+        text: 'Accompagnare Elisa a fare delle commissioni',
+        done: true,
+    },
+    {
+        text: 'Editare video per il 18',
+        done: false,
+    },
+    {
+        text: 'Uscire con Argo',
+        done: true,
+    },
+    
+]
